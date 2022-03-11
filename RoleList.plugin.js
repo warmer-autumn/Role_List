1003c1cbf54f32975ec9c7b60ff5b72508129b43
warmer-autumn <osharebones@protonmail.com>
2022-03-11 16:36:25 -0600

    Initiating repo

---
+++ RoleList.plugin.js
@@ -0,0 +1,9 @@
+/**
+ * @name RoleList
+ * @author Autumn Rain
+ * @authorId 
+ * @version 0.0.1
+ * @description A plugin to allow checkbox selection of roles when re-organizing.
+ * @website https://github.com/warmer-autumn
+ */
+
